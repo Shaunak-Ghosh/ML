d080b6764c34314370dd82b162a80badf9a99f80
Shaunak Ghosh <78599534+hacker-code237@users.noreply.github.com>
2021-02-08 16:23:40 +0000

    Update and push config.py

--- config.py
+++ config.py
@@ -1,8 +1,24 @@
 from setuptools import setup
 
 setup(
-  __project__ = "ml"
-  __version__ = "2.0.5"
-  __description__ = "a Python module that teaches a computer to read pixelated images"
-  __packages__ = ["os, tensorflow, fore, matplotlib, numpy"]
+  __project__ = "ml",
+  __version__ = "2.0.5",
+  __description__ = "a Python module that teaches a computer to read pixelated images",
+  __packages__ = ["os, tensorflow, fore, matplotlib, numpy"],
+  __author__ = "Shaunak Ghosh",
+  __author_email__ = "shaunakg2011@gmail.com"
+  __classifiers__ = [
+    "Development Status :: 5 - Stable",
+    "Intended Audience :: Education",
+    "Programming Language :: Python :: 4",
+  ],
+  Environment :: Console
+  Environment :: Console :: Wget
+  Environment :: Console :: Flask
+  Environment :: Console :: TensorFlow
+  Environment :: Console :: Numpy
+  Environment :: Console :: Fore
+  Environment :: Console :: Os
+  Environment :: GPU
+  __requires__ = ["TensorFlow, Numpy, Pip3, Pip, Npm, Mvn, Guizero"]
 )
