7dc197d26676af70357f0e640f9b84fb58ba5f58
Shaunak Ghosh <78599534+hacker-code237@users.noreply.github.com>
2021-02-08 17:58:49 +0000

    Update and push config.sh

--- config.sh
+++ config.sh
@@ -2,6 +2,8 @@
 
 # Config exit status 0
 
+echo "Welcome, " $USER
+
 sudo python3 setup.py install
 
 python3 setup.py sdist
@@ -10,8 +12,35 @@ twine upload *
 
 username: Shaunak-Ghosh
 
-Password: *******************
+Password: $secrets:PYPI_PASS
 
 pip3 install ml.git
 
+while [ 1 ]
+do
+    foo
+    sleep 2
+done
+
+npm init
+
+'npm WARN npm npm does not support Node.js v10.15.2
+npm WARN npm You should probably upgrade to a newer version of node as we
+npm WARN npm cant make any promises that npm will work with this version.
+npm WARN npm Supported releases of Node.js are the latest release of 4, 6, 7, 8, 9.
+npm WARN npm You can find the latest version at https://nodejs.org/
+This utility will walk you through creating a package.json file.
+It only covers the most common items, and tries to guess sensible defaults.
+
+See `npm help json` for definitive documentation on these fields
+and exactly what they do.
+
+Use `npm install <pkg>` afterwards to install a package and
+save it as a dependency in the package.json file.
+
+Press ^C at any time to quit.'
+package name: (@hacker-code237/ml): status.npm
+
 # Exit 0
+
+exit 0
