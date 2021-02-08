c8dcf88d9b6835507c9f2674491137c67098d13a
Shaunak Ghosh <78599534+hacker-code237@users.noreply.github.com>
2021-02-08 15:53:44 +0000

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+  __project__ = "ml"
+  __version__ = "2.0.5"
+  __description__ = "a Python module that teaches a computer to read pixelated images"
+  __packages__ = ["os, tensorflow, fore, matplotlib, numpy"]
+)
